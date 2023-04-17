4ccacf7344d5a858620ff8349b6218e6c90b8803
Raymond Oluoch <rasholuoch@gmail.com>
2023-04-17 14:49:54 +0300

    dashboard ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,29 @@
-import { CssBaseline, ThemeProvider } from "@mui/material"
+import { CssBaseline, ThemeProvider, Box } from "@mui/material"
 import { createTheme } from "@mui/material/styles"
 import { useMemo } from "react"
 import { themeSettings } from "@/theme"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Navbar from "@/scences/navbar"
+import Dashboard from "@/scences/dashboard"
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), [])
 
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        hi
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/predictions" element={<div>predictions</div>} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   )
 }
